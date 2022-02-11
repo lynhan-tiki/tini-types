57556cdcae0049cb64c92b75051735b595c639d0
Thien Ly <lyluongthien1002@gmail.com>
2022-02-11 10:31:54 +0700

    feat: map component methods

--- types/component.d.ts
+++ types/component.d.ts
@@ -1,122 +1,128 @@
 /// <reference no-default-lib="true"/> 
 
 
-type AnyObject =  Record<string,any>
- 
+type AnyObject = Record<string, any>
 
-  interface UserDefineMethod<SCOPE, P extends readonly any[] = any[], RETURN = void> {
-    (
-      this: SCOPE,
-      ...params: P
-    ): RETURN;
-  }
 
-   
-  interface UpdateMethods<DATA>{
-    /**
-     * function that updates `this.data` and trigger update UI
-     * @param updater the whole or the partial of new this.data or any plain Object
-     * @param callback callback will be triggerred after updated this.data and UI
-     */
-    setData(updater: Partial<DATA> & AnyObject, callback?: (data: DATA) => void): Promise<void>;
-        /**
-     * function that updates `this.data` and trigger update UI
-     * @param updater function that return the whole or the partial of new this.data or any plain Object
-     * @param callback callback will be triggerred after updated this.data and UI
-     */
-    setData(updater: (data: DATA) => Partial<DATA> & AnyObject, callback?: (data: DATA) => void): Promise<void>;
-  }
 
-  type FunctionRef<REF, DATA, PROPS, M> =
-    Omit<
-        REF,
-        "methods" | "onInit" | "deriveDataFromProps" | "didMount" | "didUpdate" | "didUnmount"
-      >
-    & UpdateMethods<DATA>
-    & { props: PROPS & AnyObject}
-    & M ;
-
-  interface ComponentOptions<DATA, PROPS, M> {
-    /**
-     * @property data Object which binded one way to the UI
-     */
-    data?: DATA ;
-    /**
-     * @property props Object which is default value of props sending from parent Node(from Component or Page)
-     */
-    props?: PROPS;
-      /**
-     * @property Object included methods defined by user
-     */
-    methods?: {
-      [key: string]: UserDefineMethod<
-        FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>,
-        [], any
-      >
-    };
-    /**
-     * @function
-     * `Component.onInit` được gọi khi Component được khởi tạo 
-     */
-    onInit?: UserDefineMethod<
-      FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>
-    >;
-
-    /**
-     * @function
-     *  `Component.deriveDataFromProps` được gọi sau khi Component được khởi tạo, hoặc khi Component nhận các props mới
-     *
-     * @param `nextProps` next props sending to component
-     * 
-     * Trong deriveDataFromProps bạn có thể
-     *  - Truy cập vào this.is, this.$id, this.$page và các thuộc tính khác
-     *  - Truy cập vào this.data, this.props
-     *  - Truy cập vào custom properties và methods
-     *  - Gọi các hàm this.setData và this.$spliceData để thay đổi data
-     *  - Sử dụng nextProps để lấy ra các thuộc tính mới sẽ được update
-     */
-    deriveDataFromProps?: UserDefineMethod<
-      FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>,
-      [nexProps: PROPS],
-      void
-    >;
-
-    /**
-     * @function
-     * `didMount` được gọi sau khi Custom Component được render lần đầu tiên.
-     * Chúng ta có thể sử dụng hàm này để trigger việc load data từ server
-     *  
-     */
-    didMount?: UserDefineMethod<
-      FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>
-    >;
-    /**
-     * @function
-   * `Component.didUpdate` được gọi sau khi data của Component được update.
-   * Hàm này được gọi mõi khi data trong Component thay đổ
-   * @param prevProps previous this.props received
-   * @param prevData previous this.data
+interface UPDATE_VIEW_DATA_METHODS<D> {
+  /**
+   * function that updates `this.data` and triggers update UI
+   * @param updater the whole or the partial of new this.data or any plain Object
+   * @param callback callback will be triggerred after updated this.data and UI
    */
-    didUpdate?: UserDefineMethod<
-      FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>,
-      [prevProps: PROPS, prevData: DATA],
-      void
-    >;
-    /**
-     * @function
-     *  
-     * `Component.didUnmount` được gọi khi Component được unmount.
-     * 
-     */
+  setData(updater: Partial<D> & AnyObject, callback?: (data: D) => void): Promise<void>;
+  /**
+* function that updates `this.data` and trigger update UI
+* @param updater function that return the whole or the partial of new this.data or any plain Object
+* @param callback callback will be triggerred after updated this.data and UI
+*/
+  setData(updater: (data: D) => Partial<D> & AnyObject, callback?: (data: D) => void): Promise<void>;
+}
 
-    didUnmount?: UserDefineMethod<
-      FunctionRef<ComponentOptions<DATA, PROPS, M>, DATA, PROPS, M>
-    >;
+type UNACCESSIBLE_KEYS_IN_COMPONENT = "methods" | "onInit" | "deriveDataFromProps" | "didMount" | "didUpdate" | "didUnmount"
+
+type ComponentFunctionRef<REF, D, P> =
+  Omit<
+    REF,
+    UNACCESSIBLE_KEYS_IN_COMPONENT
+  >
+  & UPDATE_VIEW_DATA_METHODS<D>
+  & { readonly props: P & AnyObject }
+
+interface ComponentOptions<D, P, M> {
+  /**
+   * @property data Object which is binded one way to the UI, pls make sure you only update `this.data` via `this.setData`
+   */
+  data?: D;
+  /**
+   * @property props Object which is default value of props sending from parent Node(from Component or Page)
+   */
+  props?: P;
+  /**
+ * @property methods Object included methods defined by user
+ */
+  readonly methods?: {
+    [K in keyof M]: M[K] extends number | string | undefined | null ? M[K] :
+    ((this:
+      ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+      & {
+        [K in keyof M]: M[K] extends number | string | undefined | null ? M[K] :
+        ((this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P> & M, ...params?: any[]) => any)
+      }
+      , ...params?: any[]) => any)
   }
+  // & ThisType<M & ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>>;
+
+  /**
+   * @function
+   * `Component.onInit` được gọi khi Component được khởi tạo 
+   */
+  onInit?(this:
+    ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+    & {
+      [K in keyof M]: M[K] extends number | string | undefined | null ? M[K] :
+      ((this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P> & M, ...params?: any[]) => any)
+    }
+
+  ): void;
+
+  /**
+   * @function
+   *  `Component.deriveDataFromProps` được gọi sau khi Component được khởi tạo, hoặc khi Component nhận các props mới
+   *
+   * @param `nextProps` next props sending to component
+   * 
+   * Trong deriveDataFromProps bạn có thể
+   *  - Truy cập vào this.is, this.$id, this.$page và các thuộc tính khác
+   *  - Truy cập vào this.data, this.props
+   *  - Truy cập vào custom properties và methods
+   *  - Gọi các hàm this.setData và this.$spliceData để thay đổi data
+   *  - Sử dụng nextProps để lấy ra các thuộc tính mới sẽ được update
+   */
+  deriveDataFromProps?(this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+    & { [k in keyof M]: (this: ComponentFunctionRef<REF, D, P, M>) => any },
+    nexProps: P
+  ): void;
+
+  /**
+   * @function
+   * `didMount` được gọi sau khi Custom Component được render lần đầu tiên.
+   * Chúng ta có thể sử dụng hàm này để trigger việc load data từ server
+   *  
+   */
+  didMount?(this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+    & { [k in keyof M]: (this: ComponentFunctionRef<REF, D, P, M>) => any },
+
+  ): void;
+
+  /**
+   * @function
+ * `Component.didUpdate` được gọi sau khi data của Component được update.
+ * Hàm này được gọi mõi khi data trong Component thay đổ
+ * @param prevProps previous this.props received
+ * @param prevData previous this.data
+ */
+
+  didUpdate?(this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+    & { [k in keyof M]: (this: ComponentFunctionRef<REF, D, P, M>) => any },
+    prevProps: P, prevData: D
+  ): void;
+
+  /**
+   * @function
+   *  
+   * `Component.didUnmount` được gọi khi Component được unmount.
+   * 
+   */
+
+  didUnmount?(this: ComponentFunctionRef<ComponentOptions<D, P, M>, D, P>
+    & { [k in keyof M]: (this: ComponentFunctionRef<REF, D, P, M>) => any });
+}
 
 
 
-  declare function Component<D = {}, P = {}, M = {}> 
-    (options: ComponentOptions<D, P, M>): void
-  
- 
+declare function Component<D = {}, P = {}, M = {}>
+  (options: ComponentOptions<D, P, M>): void
+
+
