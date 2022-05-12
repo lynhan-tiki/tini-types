86fb97bfda86acbb71822bd509e7601554bd7cd3
Thien Ly <79821988+lyluongthien-tiki@users.noreply.github.com>
2022-05-13 01:32:46 +0700

    fix: added console

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference no-default-lib="true"/>
 
 /// <reference path="ts-dom.d.ts" /> 
+/// <reference path="console.d.ts" /> 
 // Mini App typings
 /// <reference path="./app.d.ts" /> 
 /// <reference path="./component.d.ts" />
